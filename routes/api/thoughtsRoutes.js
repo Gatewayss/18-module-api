fd37fc930bea628b9c9dd9446f9743f1b2cb4758
Gage Norton <nortongage0@gmail.com>
2023-04-21 13:45:37 -0500

    Add comments

--- routes/api/thoughtsRoutes.js
+++ routes/api/thoughtsRoutes.js
@@ -9,18 +9,25 @@ const {
     deleteReaction
 } = require('../../controllers/thoughtController');
 
+// GET all thoughts
 router.get('/', getThoughts)
 
+// POST a new thought
 router.post('/', createThought)
 
+// GET single thought by id
 router.get('/:thoughtId', getSingleThought)
 
+// PUT a thought by id
 router.put('/:thoughtId', updateThought)
 
+// DELETE a single thought
 router.delete('/:thoughtId', deleteThought)
 
+// POST a reaction to a thought
 router.post('/:thoughtId/reactions', createReaction)
 
+// DELETE a reaction by id
 router.delete('/:thoughtId/reactions', deleteReaction)
 
 module.exports = router;
